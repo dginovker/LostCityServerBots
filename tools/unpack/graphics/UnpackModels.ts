3ebf78241b8f82bb39e890224c83d6323c84fec6
Pazaz <pazaz@itspazaz.com>
2025-11-29 15:11:02 -0500

    fix: Differentiate unreferenced models in unpacker

--- tools/unpack/graphics/UnpackModels.ts
+++ tools/unpack/graphics/UnpackModels.ts
@@ -2,8 +2,12 @@ import fs from 'fs';
 import zlib from 'zlib';
 
 import FileStream from '#/io/FileStream.js';
+import Jagfile from '#/io/Jagfile.js';
+import Packet from '#/io/Packet.js';
+
 import Environment from '#/util/Environment.js';
-import { printWarning } from '#/util/Logger.js';
+import { printDebug, printWarning } from '#/util/Logger.js';
+
 import { PackFile } from '#tools/pack/PackFileBase.js';
 import { listFilesExt } from '#tools/pack/Parse.js';
 
@@ -17,10 +21,21 @@ if (!fs.existsSync(`${Environment.BUILD_SRC_DIR}/models/_unpack`)) {
     fs.mkdirSync(`${Environment.BUILD_SRC_DIR}/models/_unpack`, { recursive: true });
 }
 
+const versionlist = new Jagfile(new Packet(cache.read(0, 5)));
+
+const models = [];
+
+const modelIndex = versionlist.read('model_index');
+if (modelIndex) {
+    for (let id = 0; id < modelIndex.length; id++) {
+        models[id] = modelIndex.g1();
+    }
+}
+
 const modelCount = cache.count(1);
 console.log(`Extracting ${modelCount} models`);
 
-for (let id = 0; id < modelCount; id++) {
+for (let id = 0; id < modelCount && id < models.length; id++) {
     if (!ModelPack.getById(id)) {
         ModelPack.register(id, `model_${id}`);
     }
@@ -32,8 +47,10 @@ for (let id = 0; id < modelCount; id++) {
     const model = cache.read(1, id);
     if (model) {
         fs.writeFileSync(destFile, zlib.gunzipSync(model));
-    } else {
+    } else if (models[id]) {
         printWarning(`Missing model ${name}`);
+    } else {
+        printDebug(`Missing unreferenced model ${name}`);
     }
 }
 
